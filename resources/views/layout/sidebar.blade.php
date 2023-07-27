c7fe9ebb05073007bf0a9d345d5974a5459e0923
zakikhairoliman89 <zakiktp56@gmail.com>
2023-07-27 14:48:25 +0700

    petama

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">Main</li>
+
+                <li>
+                    <a href="index.html" class="waves-effect">
+                        <i class="ti-home"></i><span class="badge rounded-pill bg-primary float-end"></span>
+                        <span>Dashboard</span>
+                    </a>
+
+
+                </li>
+                <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="ti-email"></i>
+                                    <span>Kategori</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="email-sepatu.html">Sepatu Futsal</a></li>
+                                    <li><a href="email-Alat.html">Alat Musik</a></li>
+                                    <li><a href="email-Alat.html">Perlengkapan Bulutangkis</a></li>
+                                    <li><a href="email-Perlengkapan.html">Perlengkapan Olahraga</a></li>
+                                </ul>
+                            </li>
+
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
