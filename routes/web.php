aea1693f95e9455a54cc8f40d175ad70fe8432b8
Aria Agus Riadi <ariaagusriadi100@gmail.com>
2023-07-27 15:07:37 +0700

    aria test

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dasboard', [dashboardController::class, 'index']);
+Route::get('/dashboard', [dashboardController::class, 'index']);
